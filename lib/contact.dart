ced28319e7a23063e1191159ebd7366ee6a5c4a1
Kapil_Mukati <kapil.mukati@yunicorn.in>
2025-09-23 12:48:21 +0530

    contact file updated

--- lib/contact.dart
+++ lib/contact.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class ContactClass extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Container(
+      height: 20,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.red,
+      child: Text("Hii"),
+    );
   }
 }
